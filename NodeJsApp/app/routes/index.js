7351e49ad57fa42bd60537799d5f351be149eb43
Roman <klimov_ru@rambler.ru>
2018-10-03 17:14:03 +0300

    working v

--- NodeJsApp/app/routes/index.js
+++ NodeJsApp/app/routes/index.js
@@ -1,4 +1,10 @@
-const formsRoutes = require('./forms_routes');
-module.exports = function(app, amqp) {
-  formsRoutes(app, amqp);
-};
+const userController = require('../controllers/user');
+const bodyParser = require('body-parser').json();
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/register', bodyParser, userController.registerUser);
+router.get('/index', userController.indexPage);
+
+module.exports = router;
